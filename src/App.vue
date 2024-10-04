cd6e88d80854aa2977ccd2df7974f1021e2e4cb2
Lane Shukhov <rl.shukhov@icloud.com>
2024-10-04 22:20:50 +0300

    init

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<script setup lang='ts'>
+import {
+  Card,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card'
+import {Button} from '@/components/ui/button'
+import {useColorMode} from "@vueuse/core";
+
+const mode = useColorMode()
+</script>
+
+<template>
+  <div class="container h-screen flex items-center justify-center">
+    <Card class="w-[350px]">
+      <CardHeader>
+        <CardTitle>Are you sure?</CardTitle>
+        <CardDescription>Deploy your new project in one-click.</CardDescription>
+      </CardHeader>
+      <CardFooter class="flex justify-between px-6 pb-6">
+        <Button variant="outline">
+          Cancel
+        </Button>
+        <Button>Deploy</Button>
+      </CardFooter>
+    </Card>
+  </div>
+</template>
